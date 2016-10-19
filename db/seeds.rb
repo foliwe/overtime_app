cc9f0adee1cc6b206c303350df42c9ba72a07e04
Foliwe <foliwe@gmail.com>
2016-10-19 23:07:29 +0100

    live data to admin page

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,15 @@
 										last_name: "Snow",
 										phone: "4322386131")
 
-puts "1 User created"
+
+@user1 = User.create(email: "test@example.com",
+										password: "password",
+										password_confirmation: "password",
+										first_name: "Kunta",
+										last_name: "Kinte",
+										phone: "4322386131")
+
+puts "2 User created"
 
 AdminUser.create(email: "admin@test.com",
 									password: "asdfasdf",
@@ -16,14 +24,22 @@ AdminUser.create(email: "admin@test.com",
 
 puts "1 Admin User created"
 
-100.times do |post|
+50.times do |post|
 	Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id, overtime_request: 2.5)
 end
 
-puts "100 Posts have been created"
 
-100.times do |audit_log|
+50.times do |post|
+	Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: @user1.id, overtime_request: 2.5)
+end
+
+
+50.times do |audit_log|
 	AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 6.days))
 end
 
-puts "100 audit logs have been created"
+20.times do |audit_log|
+	AuditLog.create!(user_id: @user1.id, status: 0, start_date: (Date.today - 6.days))
+end
+
+puts "50 audit logs have been created"
